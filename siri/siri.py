1fd8d5a763ccbd2008896360049767c26ad40ec0
[̲̅A][̲̅L][̲̅L][̲̅I][̲̅S][̲̅O][̲̅N] <73114327+AllisonOfTg@users.noreply.github.com>
2021-08-02 11:44:44 +0530

    TG - Musics 🎶

--- siri/siri.py
+++ siri/siri.py
@@ -11,11 +11,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 BUTTON1="📜 Source Code 📜"
-B2="telegram.dog/shamilhabeeb"
+B2="telegram.dog/NickxFury_bot"
 OWNER="Owner"
-GITCLONE="github.com/shamilhabeebnelli/song-bot"
+GITCLONE="https://t.me/AdhavaaBiriyaniKittiyalo"
 ABS="Developer"
-APPER="shamilhabeeb"
+APPER="NickxFury"
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
@@ -25,7 +25,7 @@ async def start(client, message):
                 [
                     InlineKeyboardButton(BUTTON1, url=GITCLONE)
                  ],[
-                    InlineKeyboardButton(OWNER, url=f"https://telegram.dog/{Config.OWNER}"),
+                    InlineKeyboardButton(OWNER, url=f"https://t.me/TG_MusicsChat"),
                     InlineKeyboardButton(ABS, url=B2)
             ]
           ]
@@ -67,7 +67,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[SmL\MwK 🇮🇳]" 
+            performer = f"[TG - Musics 🎶]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
@@ -88,7 +88,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://t.me/mwklinks">[MwK] Song-Bot</a>'
+        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://t.me/TG_MusicsChat">[TG - Musics 🎶]</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
