4319b59da9d1a12fc092a8195188160c80d8418a
[̲̅A][̲̅L][̲̅L][̲̅I][̲̅S][̲̅O][̲̅N] <73114327+AllisonOfTg@users.noreply.github.com>
2021-08-07 17:36:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Song A Song Downloader Bot Exclusively Made For TG - Musics 🎶\nJoin My Group & See What Can I Do 😎")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/5e3f155ba53fba8bde319.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/77e633996de605520a03f.jpg")
     OWNER = os.environ.get("OWNER", "TG_MusicsChat") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
