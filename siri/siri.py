91f3f43a5a4714de0ae4648ff65921caf0680877
[̲̅A][̲̅L][̲̅L][̲̅I][̲̅S][̲̅O][̲̅N] <73114327+AllisonOfTg@users.noreply.github.com>
2022-02-19 11:55:56 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -90,7 +90,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n<b>👥 Requested By:</b> {}\n📻 <b>Uploaded By:</b> <a href="https://t.me/TG_MusicsChat">[TG Musics]</a>'
+        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n<b>👥 Requested By:</b> {message.from_user.mention}\n📻 <b>Uploaded By:</b> <a href="https://t.me/TG_MusicsChat">[TG Musics]</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
